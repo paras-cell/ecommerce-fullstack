292accfc7ea68de8816d45312ba7106f16b0127d
paras <paraskumar19025@gmail.com>
2025-09-30 17:57:20 +0530

    add splash

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,9 +1,11 @@
+import React, { useState, useEffect } from "react";
 import {
   BrowserRouter as Router,
   Routes,
   Route,
   useLocation,
 } from "react-router-dom";
+
 import Intropage from "./home.jsx";
 import Menpage from "./filterpage/menpage.jsx";
 import All from "./filterpage/all.jsx";
@@ -22,22 +24,38 @@ import ProtectedRoute from "./pProtectedRoute.jsx";
 import Addresspage from "./Addresspage.jsx";
 import SavedAddresspage from "./address/savedaddress.jsx";
 import ShowAddresspage from "./address/showaddress.jsx";
-import CheckoutPage from "./payment/CheckoutPage.jsx.jsx";
+import CheckoutPage from "./payment/CheckoutPage.jsx";
 import OrderConfirmation from "./order/OrderConfirmation.jsx";
 import OrderHistory from "./order/History.jsx";
-import ProductSummary from "./order/ProductSummary";
+import ProductSummary from "./order/ProductSummary.jsx";
 import ProductPage from "./Productpage.jsx";
+import SplashScreen from "./splash.jsx";
 
 function Layout() {
   const location = useLocation();
+  const [showSplash, setShowSplash] = useState(location.pathname === "/");
+
+  useEffect(() => {
+    if (location.pathname === "/") {
+      const timer = setTimeout(() => {
+        setShowSplash(false);
+      }, 3000); // Show splash for 3 seconds
+      return () => clearTimeout(timer);
+    }
+  }, [location.pathname]);
+
   const hideHeaderRoutes = [
     "/addresss",
     "/savedaddress",
     "/payment",
     "/order-confirmation",
-  ]; // 👈 Add any route you want to exclude
+  ];
 
   const shouldShowHeader = !hideHeaderRoutes.includes(location.pathname);
+
+  if (showSplash) {
+    return <SplashScreen />;
+  }
 
   return (
     <>
@@ -45,7 +63,6 @@ function Layout() {
       {shouldShowHeader && <Header />}
       <Routes>
         <Route path="/" element={<Intropage />} />
-
         <Route path="/product/:id" element={<ProductPage />} />
         <Route path="/men" element={<Menpage />} />
         <Route path="/women" element={<Womenpage />} />
@@ -55,7 +72,6 @@ function Layout() {
         <Route path="/Accessories" element={<Accessoriespage />} />
         <Route path="/kid" element={<Kidpage />} />
         <Route path="/all" element={<All />} />
-
         <Route path="/login" element={<Loginpage />} />
         <Route path="/addresss" element={<Addresspage />} />
         <Route path="/payment" element={<CheckoutPage />} />
